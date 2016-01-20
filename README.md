65a24e8621147f2ae5f872c4fffd2aa7445de63b
Brandon Martinez <mbrandonweb@gmail.com>
2016-01-19 20:12:14 -0800

    Adding Keybase.io extension with first round of development

---
+++ README.md
@@ -0,0 +1,13 @@
+# Easy KeyBase.io Encrypt Extension
+A Google Chrome extension that uses public keys from keybase.io to encrypt messages.
+
+Easything is transparent and done completely in javascript.  While some do not like this (encrypting with JS) it's the only real option for a chrome extension.
+
+For more information on Keybase.io's kbpgp.js...
+https://github.com/keybase/kbpgp
+https://keybase.io/kbpgp
+
+For more information on the Keybase.io API...
+https://keybase.io/docs/api/1.0
+
+Sign up for KeyBase.io and start encrypting things today (Or just use PGP but that's less fun)...
---
+++ js/kbe.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  $('ul.kb-auto-complete').on('click', 'li', function() {
+    $('.keybase-account').val($(this).attr('val'));
+    $('.kb-auto-complete').text('');
+    $('ul.kb-auto-complete').hide();
+  });
+  $('.keybase-account').on('input', function() {
+    $('ul.kb-auto-complete').show();
+    $.ajax({
+      url: "https://keybase.io/_/api/1.0/user/autocomplete.json",
+      data: {
+        q: $(this).val(),
+      },
+      success: function(result) {
+        $('.kb-auto-complete').text('');
+        for (var i in result.completions) {
+          if (i > 4) {
+            break;
+          }
+          var components = result.completions[i].components;
+          var accountImage = result.completions[i].thumbnail != null ? result.completions[i].thumbnail : '/images/no_photo.png';
+          appendString = '<li val="' + components.username.val + '"><img src="' + accountImage + '"><span class="account-name">' + components.username.val + '</span></li>';
+          $('.kb-auto-complete').append(appendString);
+        }
+      }
+    });
+  })
+  $('button.encrypt-action').click(function() {
+    var account_name = $('.keybase-account').val();
+    var data = $('.to-encrypt').val();
+    $('.status').html('Grabbing Key...');
+    $.get("https://keybase.io/" + account_name + "/key.asc", function(account_pgp_key) {
+      $('.status').html($('.status').html() + ' Key found!');
+      encryptData(data, account_pgp_key);
+    }).fail(function(error) {
+      console.log(error);
+      $('.error-text').text(account_name + ' does not have a public key...');
+    });
+  });
+  $('button.copy').click(function() {
+    console.log($('.result').text());
+  });
+});
+
+function encryptData(data, account_pgp_key) {
+  $('.status').html($('.status').html() + ' Importing key...');
+  var account = false;
+  kbpgp.KeyManager.import_from_armored_pgp({
+    armored: account_pgp_key
+  }, function(err, user) {
+    if (!err) {
+      $('.status').html($('.status').html() + ' Key Imported!');
+      account = user;
+    }
+    else {
+      console.log(err, user);
+      $('.error-text').text('Oh no something bad happened... Please submit console log dump!');
+    }
+  });
+  if (account) {
+    $('.status').html($('.status').html() + ' Creating encrypted message...');
+    var params = {
+      msg:         data,
+      encrypt_for: account,
+    };
+    kbpgp.box(params, function(err, result_armored, result_raw) {
+      if (!err) {
+        $('.status').html($('.status').html() + ' Done!');
+        $('.result').text(result_armored);
+        $('.result').show();
+      }
+      else {
+        console.log(err, result_armored, result_raw);
+        $('.error-text').text('Oh no something bad happened... Please submit console log dump!');
+      }
+    });
+  }
+}
